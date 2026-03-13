b3f089575398d1914ba77aa5f0a8f289f2c58ce4
tommyj0 <thomasfj17@hotmail.com>
2026-03-13 12:29:53 +0000

    added quick install for fedora

---
+++ fedora_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo dnf install -y \
+    curl \
+    git \
+    vim \
+    build-essential \
+    cmake \
+    python3-dev \
+    python3-pip \
+    ninja-build \
+    clang \
+    gcc \
+    npm \
+    nodejs \
+    go \
+cargo \
+    wget \
+    curl \
+    unzip \
+    lua5.1-dev \
+    luajit \
+    ripgrep \
+    clangd
+    
